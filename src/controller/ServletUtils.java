b089734653f79d323edac9d34eb319db8b65e0bb
Yasen Partovski <yasen.partovski@gmail.com>
2016-09-25 01:08:20 +0300

    Adds: Servlets validate picture types (User, Destination)

--- src/controller/ServletUtils.java
+++ src/controller/ServletUtils.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ServletUtils {
 
+	private static final String[] availablePictureTypes = { ".jpg", ".jpeg", ".bmp", ".gif", ".png" };
+
 	public static void checkIfLoggedIn(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		if (request.getSession().getAttribute("user") == null) {
@@ -15,4 +17,21 @@ public class ServletUtils {
 			request.getRequestDispatcher("SignIn.jsp").forward(request, response);
 		}
 	}
+
+	public static boolean checkIfValidPictureType(String pictureName) {
+		if (pictureName != null && !pictureName.isEmpty()) {
+			String pictureType = pictureName.substring(pictureName.lastIndexOf("."));
+			for (String s : availablePictureTypes) {
+				if (pictureType.equals(s)) {
+					System.out.println("Picture type: " + pictureType);
+					return true;
+				}
+			}
+			System.out.println("Picture type: " + pictureType);
+			return false;
+		}
+		System.out.println("Picture type: null");
+		return false;
+	}
+
 }
