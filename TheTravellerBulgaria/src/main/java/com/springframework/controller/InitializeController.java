3504fdc5c5bfbd2ed2f0a6adeb906ced489bfa5f
DavidPavlov <ahil434@gmail.com>
2016-10-10 01:37:57 +0300

    initialize Manager fixed

--- TheTravellerBulgaria/src/main/java/com/springframework/controller/InitializeController.java
+++ TheTravellerBulgaria/src/main/java/com/springframework/controller/InitializeController.java
@@ -2,17 +2,19 @@ package com.springframework.controller;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.springframework.functionality.ProjectManager;
 
+@Controller
 @RequestMapping(value = "/initializeManager")
 public class InitializeController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public void initialize(HttpServletResponse response) {
-		ControllerUtils.setHeaders(response);
+
 		ProjectManager.getInstance();
 		return;
 	}
